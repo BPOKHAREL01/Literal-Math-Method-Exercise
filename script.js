7598a81cb80eb9619cba2cffe36d0204e17aae94
“bpokharel01” <bibhasa.pokharel@gmail.com>
2022-03-09 16:27:45 -0500

    Pushed LiteralMathMehodExercise

---
+++ script.js
@@ -0,0 +1,39 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+
+console.log(warmHugs.toUpperCase());
+
+console.log(warmHugs.replace("like","love"));
+warmHugs=warmHugs.replace("like","love");
+
+let beenImpaled= "Oh, look at that. I've been impaled.";
+
+console.log(beenImpaled.slice(-19));
+
+let dotDotDot="..."
+
+skullBones="I don't have a skull ${dotDotDot} or bones";
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber=Math.random();
+ //randomNumber*=3;
+randomNumber=Math.ceil(randomNumber);
+randomNumber=Math.round(randomNumber);
+randomNumber ++;
+console.log(randomNumber);
+
+// BONUS 
+
+console.log("Let it go!".repeat(3).toUpperCase().trim());
+
+let reindeers="Reindeers are better than people.";
+
+console.log(reindeers.replace("Reindeers are better than people.","Reindeers_are_better_than_people"));
+
+console.log(Math.SQRT2);
+console.log(Math.sqrt(2));
+
+let newRandomNumber= Math.ceil((Math.random()*15)+8);
+console.log(newRandomNumber);
+
